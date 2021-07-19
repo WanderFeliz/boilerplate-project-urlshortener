31c3df97465f36a5e85b1a1dbabf260976b19f32
Wander Feliz <55289565+WanderFeliz-Octagono@users.noreply.github.com>
2021-07-19 11:33:18 -0400

    final steps of the project

--- server.js
+++ server.js
@@ -2,6 +2,53 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+
+
+// database Configuration
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const { Schema } = mongoose;
+
+const shortUrlSchema = new Schema({
+  original_url: { type: String, required: true }, // String is shorthand for {type: String}
+  seq: { type:Number }
+});
+
+const UrlModel = mongoose.model('UrlModel', shortUrlSchema, 'UrlModel')
+// UrlModel.createIndexes({seq: 1}, { unique: true }, )
+
+// CRUD functions database
+const findUrl = async (query) =>{
+  const foundExistUrl = await UrlModel.findOne(query)
+  return foundExistUrl;
+}
+
+const createAndSaveUrl = async (url, seq, done) => {
+  const existUrl = await findUrl({original_url: url});
+  let shortUrl = new UrlModel({
+    original_url: url,
+    seq: seq
+  });
+  
+  if(existUrl){
+    done(null, existUrl);
+  }else{
+    const response = await shortUrl.save()
+    done(null, response);
+  }
+  
+}
+
+const getLastSequence = async () =>{
+  const foundUrlSeq = await UrlModel.find().sort({seq: -1}).limit(1)
+  return foundUrlSeq;
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,13 +57,66 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+// MiddleWare
+app.use(express.urlencoded({extended: false}))
+
+app.post('/api/shorturl', async function (req, res, next) { 
+  let customData = {};
+  
+  const urlSeq = await getLastSequence();
+  
+  if (urlSeq.length > 0){
+    customData.urlSeq = urlSeq[0]?._doc.seq + 1;
+  }else{
+    customData.urlSeq = 1;
+  }
+  res.locals.customData = customData;
+  next();
+ });
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// 
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:id', function(req, res) {
+  const {id} = req.params;
+  
+  findUrl({seq: id}).then((url)=>{
+    let {original_url} = url?._doc
+    res.redirect(original_url);
+  });
+    
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const {url} = req.body;
+  const {urlSeq} = res.locals.customData
+  let urlObj;
+  let error;
+  try{
+    urlObj = new URL(url)
+  }catch(err){
+    error = "invalid url";
+  }
+  if(error){
+    res.json({error: "invalid url"});
+  }else{
+
+    createAndSaveUrl(urlObj.origin, urlSeq, (err, data) =>{
+      if(err){
+        console.log(err);
+      } 
+      else{
+        let {original_url, seq} = data?._doc
+        res.json({ original_url: original_url, short_url: seq });
+      }
+    })
+
+    
+  }
 });
 
 app.listen(port, function() {
